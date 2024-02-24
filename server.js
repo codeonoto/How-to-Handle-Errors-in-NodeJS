7af3412b9fd4859c6893df01738164512ecb0e98
codeonoto <onoto0409@gmail.com>
2024-02-24 10:54:15 +0530

    Init

---
+++ server.js
@@ -0,0 +1,56 @@
+// 1. Import Express
+import express from 'express';
+import swagger from 'swagger-ui-express';
+import cors from 'cors';
+
+import productRouter from './src/features/product/product.routes.js';
+import userRouter from './src/features/user/user.routes.js';
+import bodyParser from 'body-parser';
+import jwtAuth from './src/middlewares/jwt.middleware.js';
+import cartRouter from './src/features/cart/cartItems.routes.js';
+import apiDocs from './swagger.json' assert { type: 'json' };
+
+// 2. Create a Server
+const server = express();
+
+// CORS policy configuration using "Header"
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', '*');
+//   res.header('Access-Control-Allow-Methods', '*');
+//   // return OK for preflight request.
+//   if (req.method == 'OPTIONS') {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
+
+// CORS policy configuration using "cors-lib"
+server.use(cors());
+
+server.use(bodyParser.json());
+
+// for all requests related to product, redirect to product routes
+// swaggerUI
+server.use('/api-docs', swagger.serve, swagger.setup(apiDocs));
+// localhost:3200/api/products
+server.use('/api/products', jwtAuth, productRouter);
+// localhost:3200/api/users
+server.use('/api/users', userRouter);
+// localhost:3200/api/cart
+server.use('/api/cart', jwtAuth, cartRouter);
+
+// 3. Defualt request handler
+server.get('/', (req, res) => {
+  res.send('Welcome to Ecommerce APIs');
+});
+
+// 4. Middleware to handle 404 requests
+server.use((req, res) => {
+  res.status(404).send('API not found');
+});
+
+// 5. Specify Port.
+const PORT = 3200;
+server.listen(PORT);
+console.log('Server is running at', PORT);
---
+++ src/features/product/product.controller.js
@@ -0,0 +1,54 @@
+import ProductModel from './product.model.js';
+
+export default class ProductController {
+  getAllProducts(req, res) {
+    const products = ProductModel.getAll();
+    res.status(200).send(products);
+  }
+
+  addProduct(req, res) {
+    // console.log(req.body)
+    // console.log('This is a post request');
+    // res.status(200).send('Post request received');
+    const { name, price, sizes } = req.body;
+    const newProduct = {
+      name,
+      price: parseFloat(price),
+      sizes: sizes.split(','),
+      imageUrl: req.file.filename,
+    }; 
+
+    const createdRecord = ProductModel.add(newProduct);
+    res.status(201).send(createdRecord);
+  }
+
+  rateProduct(req, res) {
+    const userID = req.query.userID;
+    const productID = req.query.productID;
+    const rating = req.query.rating;
+    const error = ProductModel.rateProduct(userID, productID, rating);
+    if (error) {
+      return res.status(400).send(error);
+    } else {
+      return res.status(200).send('Rating has been added');
+    }
+  }
+
+  getOneProduct(req, res) {
+    const id = req.params.id;
+    const product = ProductModel.get(id);
+    if (!product) {
+      res.status(404).send('Product Not Found');
+    } else {
+      return res.status(200).send(product);
+    }
+  }
+
+  filterProducts(req, res) {
+    const minPrice = req.query.minPrice;
+    const maxPrice = req.query.maxPrice;
+    const category = req.query.category;
+    const result = ProductModel.filter(minPrice, maxPrice, category);
+    res.status(200).send(result);
+  }
+}
